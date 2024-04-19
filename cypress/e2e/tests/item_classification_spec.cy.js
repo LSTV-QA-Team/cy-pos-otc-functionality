4a0f1cf09de104671dd07c6ee2c32b26aeb50162
LSTV-QA-Team <lstv.qateam@gmail.com>
2024-04-19 17:31:04 +0800

    Update

--- cypress/e2e/tests/item_classification_spec.cy.js
+++ cypress/e2e/tests/item_classification_spec.cy.js
@@ -1,4 +1,8 @@
+
+let visibility = []
+
 describe('Item Classification', () => {
+
 
     before(() => {
         // Clear the itemclassfile table before tests
@@ -12,57 +16,121 @@ describe('Item Classification', () => {
     
     beforeEach(() => {
         cy.login();
-        cy.navigateToModule('Master File', 'Item Classifications'); 
+        cy.navigateToModule('Master File', 'Item Classifications');
     });
 
-    it.skip('Check should be in Item Classification module', () => {
+    it('Check should be in Item Classification module', () => {
         cy.url({timeout: 10000}).should('contain', '/itemClassifications/?menfield=masterfile_itemclass');
 
-        cy.get(':nth-child(1) > .text-\\[2rem\\]').should('have.text', 'Item Classifications');
+        cy.get(':nth-child(1) > .text-\\[2rem\\]')
+          .should('have.text', 'Item Classifications');
 
-        cy.get('div.Mui-TableHeadCell-Content-Wrapper[title="Description"]').should('have.text', 'Item Classifications');
+        cy.get('div.Mui-TableHeadCell-Content-Wrapper[title="Description"]')
+          .should('have.text', 'Item Classifications');
     });
 
-    it('Check user can add valid data', () => {
+    it.only('Check if user can add valid data', () => {
         cy.fixture('item_class.json').then((data) => {
             for (const key in data){
-                cy.get('.sc-eDLKkx > .anticon > svg').should('be.visible');
 
-                cy.get('.sc-eDLKkx > .anticon > svg').click();
+                cy.wait(2000);
 
-                cy.get('.px-8').should('be.visible');
+                cy.get('.sc-eDLKkx > .anticon > svg')
+                  .should('be.visible')
+                  .click();
 
-                cy.get('.px-8').should('have.text', 'Add new item classification');
+                                        // use if/else condition or other way to check first if the element is present or not that should not stop 
+                                        // the execution.
 
+                                        // find a body element and execute a function w/ the found body element
+                                        cy.get('.px-8').then(($element) => {
+
+                                            // If locator is found
+
+                                            if ($element.text().includes('Add new items classification')) {
+
+
+                                                cy.wrap($element).should('be.visible');
+                                                cy.log('Visibility Passed');
+                                                visibility.push({ data: "passed" });
+
+
+                                            } 
+                                            
+                                            else {
+
+
+                                                cy.log('Visibility Failed');
+                                                visibility.push({ data: "failed" });
+
+                                            }
+
+                                        });
+                                      
+
+                                
                 cy.get('.mb-2').should('have.text', 'Item Classification Name *');
 
-                cy.get('#itmcladsc').should('be.enabled');
-
-                cy.get('#itmcladsc').clear();
+                cy.get('#itmcladsc')
+                  .should('be.enabled')
+                  .clear();
 
                 cy.get('#itmcladsc').type(data[key].itemClass);
 
-                cy.get('.border-blue-500').should('be.enabled');
+                cy.get('.border-blue-500')
+                  .should('be.enabled')
+                  .click();
 
-                cy.get('.border-blue-500').click();
+                cy.get('.Toastify__toast-body')
+                  .should('be.visible')
+                  .and('have.text', 'Successfully uploaded'); 
 
-                cy.get('.Toastify__toast-body').should('be.visible').contains('Successfully uploaded'); 
+                cy.get('.MuiTableBody-root').contains(data[key].itemClass).should('exist');
+            }
 
-                cy.get('.MuiTableBody-root').contains('Beverages').should('exist');
+        }).then(() => {
+
+
+            cy.log('Visiblity:', visibility)
+            cy.writeFile('cypress/fixtures/message.json', JSON.stringify(visibility))
+
+
+        })
+
+        
+        // Check the contents of the JSON file
+        cy.fixture('message.json').then((data) => {
+        
+            // Check if 'failed' is present only when there should be a failure
+            // checks if there is any entry in the  visibility array where the property is equal to 'failed'  
+
+            if (visibility.some(entry => entry.data === 'failed')) {   
+                
+                expect(data.some(entry => entry.data === 'failed')).to.be.true;
+                cy.log('Test failed because "failed" is present in the visibility array.');
+                cy.fail('Test failed');
+
+            } else { // skip assertion for 'passed'
+
+                cy.log('No failures detected!');
             }
         });
+
     });
 
-    it('Check special characters are allowed', () => {
+    it('Check if special characters are allowed', () => {
         cy.fixture('allowed_special_char.json').then((data) => {
             for (const key in data){
+
+                cy.wait(2000);
+
                 cy.get('.sc-eDLKkx > .anticon > svg').should('be.visible');
 
                 cy.get('.sc-eDLKkx > .anticon > svg').click();
 
-                cy.get('.px-8').should('be.visible');
-
-                cy.get('.px-8').should('have.text', 'Add new item classification');
+                cy.get('.px-8')
+                  .should('have.text', 'Add new item classification')
+                  .and('be.visible');
 
                 cy.get('.mb-2').should('have.text', 'Item Classification Name *');
 
@@ -76,14 +144,218 @@ describe('Item Classification', () => {
 
                 cy.get('.border-blue-500').click();
 
-                cy.get('.Toastify__toast-body').should('be.visible').contains('Successfully uploaded'); 
+                cy.get('.Toastify__toast-body')
+                  .should('be.visible')
+                  .and('have.text', 'Successfully uploaded'); 
 
                 cy.get('.MuiTableBody-root').contains(data[key].allowSpecialChar).should('exist');
             }
         });
     });
 
-    it('Check adding an empty data', () => {
+    it('Check upon adding an empty data', () => {
+        cy.get('.sc-eDLKkx > .anticon > svg').should('be.visible');
+
+        cy.get('.sc-eDLKkx > .anticon > svg').click();
+
+        cy.get('.px-8')
+          .should('have.text', 'Add new item classification')
+          .and('be.visible');
+
+        cy.get('.mb-2').should('have.text', 'Item Classification Name *');
+
+        cy.get('#itmcladsc').should('be.enabled');
+
+        cy.get('#itmcladsc').click();
+
+        // Item Class field should be 'empty'
+        cy.get('#itmcladsc').should('be.empty');
+
+        cy.get('.border-blue-500').should('be.enabled');
+
+        cy.get('.border-blue-500').click();
+
+        cy.get('.text-sm').should('be.visible');
+
+        // Validation '* is required' should be visible
+        cy.get('.text-sm').should('have.text', 'Item Classification Name * is required');
+
+        cy.get('.px-8 > .flex > .anticon > svg').click();
+    });
+
+    it('Check upon adding an existing data', () => {
+        cy.fixture('item_class.json').then((data) => {
+            for (const key in data){
+                cy.get('.sc-eDLKkx > .anticon > svg').should('be.visible');
+
+                cy.get('.sc-eDLKkx > .anticon > svg').click();
+
+                cy.get('.px-8')
+                  .should('have.text', 'Add new item classification')
+                  .and('be.visible');
+
+                cy.get('.mb-2').should('have.text', 'Item Classification Name *');
+
+                cy.get('#itmcladsc').should('be.enabled');
+
+                cy.get('#itmcladsc').clear();
+
+                cy.get('#itmcladsc').type(data[key].itemClass);
+
+                cy.get('.border-blue-500').should('be.enabled');
+
+                cy.get('.border-blue-500').click();
+
+                cy.get('.Toastify__toast-body')
+                  .should('be.visible')
+                  .and('have.text', 'Duplicate entry! Kindly check your inputs.');
+
+                cy.get('.MuiTableBody-root').find('tr').contains(data[key].itemClass)
+                  .should('have.length', 1);
+            }
+        });
+    });
+
+    it('Check if user can edit data', () => {
+        cy.fixture('item_class.json').then((data) => {
+            for (const key in data) {
+                // Should have an existing data to edit
+                // Find the table row containing the desired data to be edited
+                cy.contains('tbody > tr', data[key].itemClass).within(() => {
+                    // Click the edit button within this row
+                    cy.get('[data-icon="edit"][aria-hidden="true"]').click();
+                });
+
+                cy.get('.px-8').should('have.text', 'Beverages') // text should be ' Edit Item Classification'
+                  .and('be.visible');
+
+                cy.log("Edit modal header should be in text 'Edit Item Classification'")
+
+                cy.get('.mb-2').should('be.visible');
+
+                cy.get('#itmcladsc').should('be.enabled');
+
+                cy.get('#itmcladsc').should('have.value', data[key].itemClass);
+
+                cy.get('#itmcladsc').clear();
+
+                cy.get('#itmcladsc').type(data[key].editItemClass);
+
+                cy.get('.border-blue-500').should('be.visible');
+
+                cy.get('.border-blue-500').should('be.enabled');
+
+                cy.get('.border-blue-500').click();
+
+                cy.get('.Toastify__toast-body')
+                  .should('be.visible')
+                  .and('have.text', 'Successfully updated Record!'); 
+
+                cy.get('.MuiTableBody-root').contains(data[key].editItemClass).should('exist');
+            }
+        });
+    });
+
+    it('Check if user can search valid data', () => {
+        cy.fixture('item_class.json').then((data) => {
+            for (const key in data) {
+                cy.get('[data-testid="SearchIcon"]').should('be.visible');
+
+                cy.get('[data-testid="SearchIcon"]').click();
+
+                cy.get('#\\:rb\\:').should('be.visible');
+
+                cy.get('#\\:rb\\:').should('be.enabled');
+
+                cy.get('#\\:rb\\:').clear();
+
+                cy.get('#\\:rb\\:').type(data[key].editItemClass);
+
+                cy.get('#\\:rb\\:').type('{enter}');
+
+                cy.get('.MuiTableBody-root').contains(data[key].editItemClass).should('exist');
+            }
+        })
+    });
+
+    it('Check if user can search invalid data', () => {
+        cy.fixture('item_class.json').then((data) => {
+            for (const key in data) {
+                cy.get('[data-testid="SearchIcon"]').should('be.visible');
+
+                cy.get('[data-testid="SearchIcon"]').click();
+
+                cy.get('#\\:rb\\:').should('be.visible');
+
+                cy.get('#\\:rb\\:').should('be.enabled');
+
+                cy.get('#\\:rb\\:').clear();
+
+                cy.get('#\\:rb\\:').type('Appetizer');
+
+                cy.get('#\\:rb\\:').type('{enter}');
+
+                cy.get('p.MuiTypography-root.MuiTypography-body1.css-8tf66k-MuiTypography-root')
+                  .should('have.text', 'No records to display');
+
+            }
+        })
+    });
+
+    it('Check if user can delete data', () => {
+        cy.fixture('item_class.json').then((data) => {
+            for (const key in data) {
+                // Should have an existing data to delete
+                // Find the table row containing the desired data to be deleted
+                cy.contains('tbody > tr', data[key].editItemClass).within(() => {
+                    // Click the delete button within this row
+                    cy.get('[data-icon="delete"][aria-hidden="true"]').click();
+                });
+
+                cy.get('.px-8').should('have.text', 'Delete Confirmation')
+                  .and('be.visible');
+
+                cy.get('.h-\\[500px\\] > h1')
+                  .should('have.text', 'Do you want to delete: ' + data[key].editItemClass + ' ?');
+
+                cy.get('.border-red-500').should('be.visible');
+
+                cy.get('.border-red-500').should('be.enabled');
+
+                cy.get('.border-red-500').should('have.text', 'Confirm');
+
+                cy.get('.border-red-500').click();
+
+                cy.get('.Toastify__toast-body')
+                  .should('be.visible')
+                  .and('have.text', 'Successfully deleted Record!');
+            }
+        })
+    });
+
+    it('Check back functionality', () => {
+        cy.get(':nth-child(1) > .flex > .anticon > svg > path').should('be.visible');
+
+        cy.get(':nth-child(1) > .flex > .anticon > svg').click();
+
+        cy.get('.text-\\[3rem\\]').should('be.visible');
+        
+        cy.get('.text-\\[3rem\\]').should('have.text', 'Masterfile');
+    });
+
+    it.skip('Check print functionality', () => {
+        // print button should be enabled 
+        cy.get('.sc-guDLey.decbXQ')
+          .should('be.visible')
+          .and('be.enabled')
+
+        // click print button
+        cy.get('.sc-guDLey.decbXQ').click();
+
+    });
+
+    // Negative Testing
+    it('Check if text field have maximum length', () => {
         
     });
 });
--- xlsx_converter_to_json.js
+++ xlsx_converter_to_json.js
@@ -1,12 +1,12 @@
 var xlsx = require("xlsx");
 var fs = require('fs');
-var dataPathExcel = "cypress/fixtures/excel_data.xlsx";
-var wb = xlsx.readFile(dataPathExcel);
+var dataPathExcel = "cypress/fixtures/excel_data.xlsx"; //path to Excel file
+var wb = xlsx.readFile(dataPathExcel);   //reading the Excel file into a workbook object (`wb`).
 
-wb.SheetNames.forEach(function(sheetName) {
-    var sheetValue = wb.Sheets[sheetName];
-    var excelData = xlsx.utils.sheet_to_json(sheetValue);
-    var dataPathJson = "cypress/fixtures/";
+wb.SheetNames.forEach(function(sheetName) {  //loopimg through each sheet in excel file
+    var sheetValue = wb.Sheets[sheetName];  //getting data of current sheet
+    var excelData = xlsx.utils.sheet_to_json(sheetValue);    //converting the sheet to JSON file format
+    var dataPathJson = "cypress/fixtures/";  //path where JSON file will be saved
 
     // Using the sheet name for the JSON file name
     fs.writeFile(`${dataPathJson}${sheetName}.json`, JSON.stringify(excelData), function(err) {
@@ -17,5 +17,10 @@ wb.SheetNames.forEach(function(sheetName) {
         }
     });
 });
+
+// `${dataPathJson}${sheetName}.json` 
+// Path of the JSON file to be created, where `${sheetName}` is the name of the current sheet.
+
+
 
 //requirements: npm install xlsx in terminal
