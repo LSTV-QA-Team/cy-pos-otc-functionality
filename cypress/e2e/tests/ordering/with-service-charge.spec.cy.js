e9e6a9de4585fbfa3e175a7b86d8eca8286394b8
lstvadrian <149146578+lstvadrian@users.noreply.github.com>
2024-06-18 13:25:02 +0800

    with service charge done
    with service charge done

--- cypress/e2e/tests/ordering/with-service-charge.spec.cy.js
+++ cypress/e2e/tests/ordering/with-service-charge.spec.cy.js
@@ -37,12 +37,11 @@ describe("With Service Charge ", () => {
 
     cy.get(".ml-5 > :nth-child(2) > :nth-child(4)").should(
       "have.text",
-      "Service Charge 6.79"
+      "Service Charge " + T1_SCharge
     );
     cy.get("#customerName").click().type("Edith");
     cy.get(".border-blue-500").click();
 
-    cy.get(".mb-5 > :nth-child(2)").should("have.text", "Balance₱0.00");
     cy.get(".my-5 > .grid > :nth-child(1) > .text-green-700").click();
     cy.contains("Transaction Complete.").should(
       "have.text",
@@ -274,7 +273,7 @@ describe("With Service Charge ", () => {
     cy.contains("Amazing Aloha Yumburger").click().wait(1000);
 
     cy.contains("Add Discount").click();
-    cy.get("#discde").select("Person with Disablity"); // SHOULD BE DISABILITY
+    cy.get("#discde").select("Person with Disability"); // SHOULD BE Disability
     cy.get("#orderitmid").click();
     cy.get(".border-blue-500").click();
 
@@ -694,5 +693,278 @@ describe("With Service Charge ", () => {
       "have.text",
       "Transaction Complete."
     );
+  });
+
+  it("1 Pax with MEMC PWD Discount and Service Charge", () => {
+    cy.get(".px-8").should("have.text", "Select Pricelist").wait(2000);
+    cy.get("#postypcde").select("TAKEOUT").wait(2000);
+    cy.get("#warcde").select("TAKE-OUT PRICE").wait(2000);
+    cy.contains("Proceed").click();
+
+    cy.contains("FOOD").click().wait(1000);
+    cy.contains("Family Super Meals").click().wait(1000);
+    cy.contains(
+      "FSM A 8pc: Chickenjoy Bucket (4 Rice, 4 Sides, 4 Mini Sundaes, and 4 Regular Drinks)"
+    )
+      .click()
+      .wait(1000);
+
+    cy.contains("Add Discount").click();
+    cy.get("#discde").select("Person with Disability");
+    cy.get("#orderitmid0").click();
+    cy.get(".border-blue-500").click();
+
+    cy.get("#cardholder").click().type("Ariana Grande");
+    cy.get("#cardno").click().type("464345");
+    cy.get("#tin").click().type("7734625");
+    cy.get("#discountUser > .flex-col > #buttons > .border-blue-500").click();
+    cy.get(":nth-child(2) > .MuiTableCell-root > .flex > .ml-10").should(
+      "have.text",
+      "Discount : PWD"
+    );
+
+    const ST = 799;
+    const Disc_Formula = Number(200 / 1.12) * 0.2;
+    const Discount = Disc_Formula.toFixed(2); //35.71
+    const LVA = (200 / 1.12) * 0.12;
+    const LVA1 = Number(LVA.toFixed(2));
+    const SC_Formula = (ST / 1.12) * 0.1;
+    const T10_SCharge = Number(SC_Formula.toFixed(2)); //71.34
+    const SCharge_dsc = T10_SCharge * 0.2;
+    const SCharge_dsc1 = SCharge_dsc.toFixed(2);
+    const GT = Number(ST - Discount - LVA1);
+    const total = Number(GT + T10_SCharge - SCharge_dsc1);
+    const total1 = Number(total.toFixed(2));
+
+    cy.get(".bg-black > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "799.00"
+    );
+    cy.get(".bg-black > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      Discount
+    );
+    cy.get(".bg-black > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      LVA1
+    );
+    cy.get(":nth-child(4) > :nth-child(2)").should("have.text", T10_SCharge);
+    cy.get(":nth-child(5) > :nth-child(2)").should("have.text", SCharge_dsc1);
+    cy.get(".font-extrabold > :nth-child(2)").should("have.text", total1);
+
+    cy.contains("Payment").click();
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "₱799.00"
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      "-" + Discount
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      "-" + LVA1
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(4)").should(
+      "have.text",
+      "Service Charge " + T10_SCharge
+    );
+
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(5)").should(
+      "have.text",
+      "SCharge Discount -" + SCharge_dsc1
+    );
+    cy.get(".text-red > :nth-child(2)").should("have.text", "₱" + total1);
+
+    cy.contains("CASH").click();
+    cy.get("#customerName").click().type("Ariana G");
+    cy.get(".border-blue-500").click();
+    cy.get(".my-5 > .grid > :nth-child(1) > .text-green-700").click();
+    cy.contains("Transaction Complete.").should(
+      "have.text",
+      "Transaction Complete."
+    );
+  });
+
+  it("1 Pax with MEMC Senior Discount and Service Charge", () => {
+    cy.get(".px-8").should("have.text", "Select Pricelist").wait(2000);
+    cy.get("#postypcde").select("DINE IN").wait(2000);
+    cy.get("#warcde").select("DINE-IN PRICE").wait(2000);
+    cy.contains("Proceed").click();
+
+    cy.contains("FOOD").click().wait(1000);
+    cy.contains("Chicken").click().wait(1000);
+    cy.contains("Chickenjoy Bucket 6pcs").click();
+
+    cy.contains("Add Discount").click();
+    cy.get("#discde").select("Senior");
+    cy.get("#orderitmid0").click();
+    cy.get(".border-blue-500").click();
+
+    cy.get("#cardholder").click().type("Jessie J");
+    cy.get("#cardno").click().type("3452435");
+    cy.get("#tin").click().type("32556236");
+    cy.get("#discountUser > .flex-col > #buttons > .border-blue-500").click();
+    cy.get(":nth-child(2) > .MuiTableCell-root > .flex > .ml-10").should(
+      "have.text",
+      "Discount : Senior"
+    );
+
+    const ST = 399;
+    const Disc_Formula = Number(ST / 1.12) * 0.2;
+    const Discount = Disc_Formula.toFixed(2); //71.25
+    const LVA = (ST / 1.12) * 0.12;
+    const LVA1 = Number(LVA.toFixed(2));
+    const SC_Formula = (ST / 1.12) * 0.1;
+    const T11_SCharge = Number(SC_Formula.toFixed(3)); //35.625
+    const SCharge_dsc = T11_SCharge * 0.2;
+    const SCharge_dsc1 = SCharge_dsc.toFixed(2);
+    const GT = Number(ST - Discount - LVA1);
+    const total = Number(GT + T11_SCharge - SCharge_dsc1);
+    const total1 = Number(total.toFixed(2));
+
+    cy.get(".bg-black > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "399.00"
+    );
+    cy.get(".bg-black > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      Discount
+    );
+    cy.get(".bg-black > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      LVA1
+    );
+    cy.get(":nth-child(4) > :nth-child(2)").should(
+      "have.text",
+      Math.round(T11_SCharge * 100) / 100
+    );
+    cy.get(":nth-child(5) > :nth-child(2)").should(
+      "have.text",
+      Math.round(SCharge_dsc1 * 100) / 100
+    );
+    cy.get(".font-extrabold > :nth-child(2)").should("have.text", total1 + "0");
+
+    cy.contains("Payment").click();
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "₱399.00"
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      "-" + Discount
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      "-" + LVA1
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(4)").should(
+      "have.text",
+      "Service Charge " + Math.round(T11_SCharge * 100) / 100
+    );
+
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(5)").should(
+      "have.text",
+      "SCharge Discount -" + Math.round(SCharge_dsc1 * 100) / 100
+    );
+    cy.get(".text-red > :nth-child(2)").should("have.text", "₱" + total1 + "0");
+
+    cy.contains("CASH").click();
+    cy.get("#customerName").click().type("Ariana G");
+    cy.get(".border-blue-500").click();
+    cy.get(".my-5 > .grid > :nth-child(1) > .text-green-700").click();
+    cy.contains("Transaction Complete.").should(
+      "have.text",
+      "Transaction Complete."
+    );
+  });
+
+  it("1 Pax with MEMC PWD Discount and Service Charge", () => {
+    cy.get(".px-8").should("have.text", "Select Pricelist").wait(2000);
+    cy.get("#postypcde").select("DINE IN").wait(2000);
+    cy.get("#warcde").select("DINE-IN PRICE").wait(2000);
+    cy.contains("Proceed").click();
+
+    cy.contains("FOOD").click().wait(1000);
+    cy.contains("Chicken").click().wait(1000);
+    cy.contains("Chickenjoy Bucket 8pcs").click();
+
+    cy.contains("Add Discount").click();
+    cy.get("#discde").select("Person with Disability");
+    cy.get("#orderitmid0").click();
+    cy.get(".border-blue-500").click();
+
+    cy.get("#cardholder").click().type("Nicky Minaj");
+    cy.get("#cardno").click().type("98756790");
+    cy.get("#tin").click().type("90834");
+    cy.get("#discountUser > .flex-col > #buttons > .border-blue-500").click();
+    cy.get(":nth-child(2) > .MuiTableCell-root > .flex > .ml-10").should(
+      "have.text",
+      "Discount : PWD"
+    );
+
+    const ST = 499;
+    const Disc_Formula = Number(ST / 1.12) * 0.2;
+    const Discount = Disc_Formula.toFixed(2); //89.11
+    const LVA = (ST / 1.12) * 0.12;
+    const LVA1 = Number(LVA.toFixed(2));
+    const SC_Formula = (ST / 1.12) * 0.1;
+    const T12_SCharge = Number(SC_Formula.toFixed(2)); //44.55
+    const SCharge_dsc = T12_SCharge * 0.2;
+    const SCharge_dsc1 = SCharge_dsc.toFixed(2);
+    const GT = Number(ST - Discount - LVA1);
+    const total = Number(GT + T12_SCharge - SCharge_dsc1);
+    const total1 = Number(total.toFixed(2));
+
+    cy.get(".bg-black > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "499.00"
+    );
+    cy.get(".bg-black > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      Discount
+    );
+    cy.get(".bg-black > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      LVA1
+    );
+    cy.get(":nth-child(4) > :nth-child(2)").should("have.text", T12_SCharge);
+    cy.get(":nth-child(5) > :nth-child(2)").should("have.text", SCharge_dsc1);
+    cy.get(".font-extrabold > :nth-child(2)").should("have.text", total1);
+
+    cy.contains("Payment").click()
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(1) > :nth-child(2)").should(
+      "have.text",
+      "₱499.00"
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(2) > :nth-child(2)").should(
+      "have.text",
+      "-" + Discount
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(3) > :nth-child(2)").should(
+      "have.text",
+      "-" + LVA1
+    );
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(4)").should(
+      "have.text",
+      "Service Charge " + T12_SCharge
+    );
+
+    cy.get(".ml-5 > :nth-child(2) > :nth-child(5)").should(
+      "have.text",
+      "SCharge Discount -" + SCharge_dsc1
+    );
+    cy.get(".text-red > :nth-child(2)").should("have.text", "₱" + total1);
+
+    cy.contains("CASH").click();
+    cy.get("#customerName").click().type("Ariana G");
+    cy.get(".border-blue-500").click();
+    cy.get(".my-5 > .grid > :nth-child(1) > .text-green-700").click();
+    cy.contains("Transaction Complete.").should(
+      "have.text",
+      "Transaction Complete."
+    );
+
+
   });
 });
