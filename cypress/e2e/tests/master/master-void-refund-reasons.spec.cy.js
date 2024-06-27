bcba858b6aaed03787d08b7a91cfe57433bb9991
LSTV-QA <lstv.qateam@gmail.com>
2024-06-28 03:57:03 +0800

    Run 2 Void Refund Reasons

--- cypress/e2e/tests/master/master-void-refund-reasons.spec.cy.js
+++ cypress/e2e/tests/master/master-void-refund-reasons.spec.cy.js
@@ -35,41 +35,41 @@ describe('Void/Refund Reasons', () => {
 
     })
 
-    // after(() => {
+    after(() => {
 
-    //     cy.fixture('data-to-delete.json').then((data) => {
+        cy.fixture('data-to-delete.json').then((data) => {
            
-    //         data.forEach((item) => {
+            data.forEach((item) => {
 
-    //             const specialChar = item.dataToDelete;
-    //             const deleteQuery = `DELETE FROM voidreasonfile WHERE voidcde = '${specialChar}'`;
+                const specialChar = item.dataToDelete;
+                const deleteQuery = `DELETE FROM voidreasonfile WHERE voidcde = '${specialChar}'`;
                 
-    //             cy.task('queryDb', deleteQuery).then(() => {
+                cy.task('queryDb', deleteQuery).then(() => {
 
-    //                 cy.log(`Deleted data with description: ${specialChar}`)
+                    cy.log(`Deleted data with description: ${specialChar}`)
 
-    //             })
-    //         })
+                })
+            })
     
            
-    //         cy.task('queryDb', 'SELECT * FROM voidreasonfile').then((records) => {
+            cy.task('queryDb', 'SELECT * FROM voidreasonfile').then((records) => {
 
-    //             const remainingData = records.map((record) => record.description);
-    //             const deletedChars = data.map((item) => item.dataToDelete);
+                const remainingData = records.map((record) => record.description);
+                const deletedChars = data.map((item) => item.dataToDelete);
                 
 
-    //             deletedChars.forEach((char) => {
+                deletedChars.forEach((char) => {
 
-    //                 expect(remainingData).to.not.include(char)
+                    expect(remainingData).to.not.include(char)
 
-    //             })
+                })
     
-    //             cy.log('Specified data Successfully deleted.')
-    //         })
-    //     })
-    // })
+                cy.log('Specified data Successfully deleted.')
+            })
+        })
+    })
 
-    it.only('Check Void/Refund Reasons Page', () => { 
+    it('Check Void/Refund Reasons Page', () => { 
         
         cy.navigateToModule('Master File', 'Void/Refund Reasons')
 
@@ -99,13 +99,15 @@ describe('Void/Refund Reasons', () => {
         cy.checkForFailure(assertionResults, failureMessages)
     });
 
-    it.only('Add Functionality', () => {
+    it('Add Functionality', () => {
 
         cy.fixture('master-voidreasons-data.json').then((data) => {
 
             cy.wait(4000) 
 
             cy.get('.sc-eDLKkx > .anticon > svg').click()
+
+            cy.wait(2000)
                 
             cy.checkElementVisibility('.shadow-lg', '2.1', 'Upon Clicking the "Save" button:', '"Add Void/Refund Reasons" modal window was not visible or active.', assertionResults, failureMessages)
 
@@ -148,10 +150,12 @@ describe('Void/Refund Reasons', () => {
 
                         cy.get('.border-blue-500').click()
 
+                        cy.wait(2000)
+
                         cy.checkLabelCaption('.Toastify__toast-body', '13.1', 'Upon Clicking the "Save" button:', 'Duplicate entry! Kindly check your inputs', assertionResults, failureMessages) 
                     } 
                     
-                    else if ($input.val() === "Item Out of Stock") {
+                    else if (data[key].voidReasons === "Item Out of Stock") {
 
                         cy.get('#voidcde').clear().type(data[key].voidReasons)
 
@@ -184,17 +188,17 @@ describe('Void/Refund Reasons', () => {
 
                     }
 
-                    else if ($input.val() === "% & ( ) / - .") {
+                    else if (data[key].voidReasons === "% & ( ) / - .") {
 
                         cy.get('#voidcde').clear().type(data[key].voidReasons)
 
                         cy.get('.border-blue-500').click()
                         
-                        cy.wait(4000)
+                        cy.wait(2000)
 
                         cy.checkLabelCaption('.Toastify__toast-body', '9.1', 'Upon Clicking the "Save" button:', 'Successfully saved.', assertionResults, failureMessages) 
 
-                        cy.checkElementInvisibility('.shadow-lg', '9.2.1', 'Upon clicking the "OK" button:', 'The "Add Void/Refund Reasons" modal window was not visible or active.', assertionResults, failureMessages)
+                        cy.checkElementVisibility('.shadow-lg', '9.2.1', 'Upon clicking the "OK" button:', 'The "Add Void/Refund Reasons" modal window was not visible or active.', assertionResults, failureMessages)
 
                         // 11.2.2 Check if the "Description" textbox object is cleared or blank.
 
@@ -202,11 +206,13 @@ describe('Void/Refund Reasons', () => {
 
                     }
 
-                    else if ($input.val() === "This is a very long string that exceeds the maximum allowed length.") {
+                    else if (data[key].voidReasons === "This is a very long string that exceeds the maximum allowed length.") {
 
                         cy.get('#voidcde').clear().type(data[key].voidReasons)
 
-                        cy.checkElementVisibility('.text-sm', '17.1', 'Upon encoding data:', 'The validation message for "Please limit your input to 50 characters." was not visible.', assertionResults, failureMessages)
+                        cy.checkElementVisibility('.Toastify__toast-body', '17.2', 'Upon encoding data:', 'The validation message for "Please limit your input to 50 characters." was not visible.', assertionResults, failureMessages)
+
+                        cy.checkInputMaxLength('#voidcde', 50, '17.1', 'Upon Encoding in "Card Type" Textbox:', assertionResults, failureMessages)
 
                         cy.get('.border-blue-500').click()
 
@@ -218,7 +224,7 @@ describe('Void/Refund Reasons', () => {
 
                     }
 
-                    else if ($input.val() === "© ™ ® à á â ñ ä ¢ £ ¥ € ! @ # $ ^ * _ + = < > ? ` ~ \" | \\ [ ] ; :") {
+                    else if (data[key].voidReasons === "© ™ ® à á â ñ ä ¢ £ ¥ € ! @ # $ ^ * _ + = < > ? ` ~ \" | \\ [ ] ; :") {
 
                         cy.get('#voidcde').clear().type(data[key].voidReasons)
 
@@ -226,11 +232,11 @@ describe('Void/Refund Reasons', () => {
 
                         cy.get('.border-blue-500').click()
 
-                        cy.wait(4000)
+                        cy.wait(2000)
 
-                        cy.checkLabelCaption('.Toastify__toast-body', '15.1', 'Upon Clicking the "Save" button:', 'Please use only the following approved special characters: % & ( ) / - .', assertionResults, failureMessages) 
+                        // cy.checkLabelCaption('.Toastify__toast-body', '15.1', 'Upon Clicking the "Save" button:', 'Please use only the following approved special characters: % & ( ) / - .', assertionResults, failureMessages) 
 
-                        cy.checkElementInvisibility('.shadow-lg', '15.2.1', 'Upon clicking the "OK" button:', 'The "Add Void/Refund Reasons" modal window was visible or active.', assertionResults, failureMessages)
+                        cy.checkElementVisibility('.shadow-lg', '15.2.1', 'Upon clicking the "OK" button:', 'The "Add Void/Refund Reasons" modal window was visible or active.', assertionResults, failureMessages)
 
                         cy.wait(4000)
                         
@@ -242,9 +248,11 @@ describe('Void/Refund Reasons', () => {
 
                         cy.get('.border-blue-500').click()
 
-                        cy.wait(4000)
+                        cy.wait(2000)
 
                         cy.checkLabelCaption('.Toastify__toast-body', '4.1', 'Upon Clicking the "Save" button:', 'Successfully saved.', assertionResults, failureMessages) 
+
+                        cy.wait(2000)
                         
                         cy.checkElementVisibility('.shadow-lg', '4.2.1', 'Upon Clicking the "Save" button:', 'The "Add Void/Refund Reasons" modal window was not visible or active.', assertionResults, failureMessages)
 
@@ -265,6 +273,8 @@ describe('Void/Refund Reasons', () => {
 
     it('Edit Functionality', () => {
 
+        cy.get('.border-red-500').click()
+
         cy.fixture('master-voidreasons-data.json').then((data) => {
 
             const specificVoidReasons = data[7];
@@ -276,6 +286,8 @@ describe('Void/Refund Reasons', () => {
                     cy.get('[data-icon="edit"][aria-hidden="true"]').click()
 
                 })
+
+                cy.wait(2000)
 
                 cy.checkElementVisibility('.shadow-lg', '20.1', 'Upon Clicking the "Edit" button:', 'The "Edit Void/Refund Reasons" modal window was not visible or active.', assertionResults, failureMessages)
 
@@ -332,7 +344,7 @@ describe('Void/Refund Reasons', () => {
                     
                     cy.checkLabelCaption('.h-\\[500px\\] > h1', '24.3', 'Upon clicking the "Delete" button on pager UI', 'Do you want to delete: ' + data[key].voidReasons + ' ?', assertionResults, failureMessages);
 
-                    cy.contains('button[class*="border-blue-500"]', 'Cancel').click()
+                    cy.get('.border-blue-500').click()
 
                     cy.wait(3000)
 
@@ -344,7 +356,7 @@ describe('Void/Refund Reasons', () => {
 
                     })
 
-                    cy.contains('button[class*="border-red-500"]', 'Confirm').click()
+                    cy.get('.border-red-500').click()
 
                     cy.wait(3000)
 
