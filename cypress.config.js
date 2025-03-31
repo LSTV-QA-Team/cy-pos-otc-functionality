f9ccd7a1c89b6567383398077d9aea53390c14c2
lstvcristel <lstv.cristel.millan@gmail.com>
2025-03-31 13:23:53 +0800

    [Organizing]: Removing unnecessary reports and

--- cypress.config.js
+++ cypress.config.js
@@ -9,34 +9,21 @@ const path = require('path');
 const moment = require('moment');
 const { exec } = require('child_process');
 
-
 module.exports = defineConfig({
-
   env: {
-
     userCode: 'lstv',
     userPassword: 'lstventures',
-    
     db: {
       host: process.env.DB_HOST,
-      user: process.env.DB_USER, 
-      password: process.env.DB_PASSWORD, 
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     }
   },
 
   redirectionLimit: 1000,
-
-  // experimentalMemoryManagement: true,
-
-  // defaultCommandTimeout: 4000,
-
-  // pageLoadTimeout: 100000,
-
   watchForFileChanges: false,
-
   hideXHRInCommandLog: true,
-
   video: false,
 
   reporter: 'cypress-mochawesome-reporter',
@@ -45,34 +32,32 @@ module.exports = defineConfig({
     charts: true,
     reportPageTitle: 'Cypress POS-OTC Functionality Report',
     reportFilename: `[status]-[name]-${moment().format('YYYY-MM-DD')}-report`,
-    embeddedScreenshots: false,
+    embeddedScreenshots: true,
     inlineAssets: true,
-    overwrite: true,
+    saveJson: true,
     json: true,
     html: true,
-    debug: false,
-    quiet: true,
-    videoOnFailOnly: true,
-    code: false,
-    saveAllAttempts: false,
-    screenshotOnRunFailure: false,
+    overwrite: false,
+    quiet: false,
+    screenshotOnRunFailure: true,
   },
 
   e2e: {
-
     baseUrl: 'http://localhost:5173/#/pages',
     experimentalStudio: true,
     testIsolation: false,
     experimentalRunAllSpecs: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    execTimeout: 60000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 60000,
+    responseTimeout: 60000,
+    taskTimeout: 60000,
 
-    // implement node event listeners here
     setupNodeEvents(on, config) {
-
-      // config.specPattern = [
-      //   'cypress/e2e/tests/master',
-      //   'cypress/e2e/tests/chashiering',
-      //   'cypress/e2e/tests/ordering',
-      // ]
 
       config.specPattern = [
         'cypress/e2e/tests/master/01-master-item-class.spec.cy.js',
@@ -92,12 +77,9 @@ module.exports = defineConfig({
         'cypress/e2e/tests/master/15-master-header.spec.cy.js',
         'cypress/e2e/tests/master/16-master-other-charges.spec.cy.js',
         'cypress/e2e/tests/master/17-master-system.parameter.spec.cy.js',
-        'cypress/e2e/tests/master/18-master-cash-in-out-reason.spec.cy.js',
-
         'cypress/e2e/tests/cashiering/cashiering-cashfund.spec.cy.js',
         'cypress/e2e/tests/cashiering/cashiering-cash-in.spec.cy.js',
         'cypress/e2e/tests/cashiering/cashiering-cash-out.spec.cy.js',
-
         'cypress/e2e/tests/ordering/01-ordering.spec.cy.js',
         'cypress/e2e/tests/ordering/02-ordering-negative.spec.cy.js',
         'cypress/e2e/tests/ordering/03-transaction.spec.cy.js',
@@ -134,123 +116,76 @@ module.exports = defineConfig({
         'cypress/e2e/tests/ordering/34-transaction.spec.cy.js',
         'cypress/e2e/tests/ordering/35-transaction.spec.cy.js',
         'cypress/e2e/tests/ordering/36-transaction.spec.cy.js',
-
         'cypress/e2e/tests/cashiering/cashiering-cash-declaration.spec.cy.js',
-      ]
+      ];
 
       require('cypress-mochawesome-reporter/plugin')(on);
-      
 
-          on('before:browser:launch', (browser = {}, launchOptions) => {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log(launchOptions.args);
 
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--no-sandbox', '--disable-gpu');
+        }
 
-            console.log(launchOptions.args)
-        
-            if (browser.name == 'chrome') {
-              launchOptions.args.push(['--no-sandbox','--disable-gpu'])
-            }
-        
-            return launchOptions
-
-          })
-
-
-
-      // Define the "queryDb" task9
-      on("task", {
-        queryDb: (query) => {
-
-          return queryTestDb(query, config);
-
-        },
+        return launchOptions;
       });
 
-
-      // verify downloded file
       on('task', {
+        queryDb: (query) => queryTestDb(query, config),
         verifyDownloads: (downloadsPath) => {
-
           try {
-            return fs.readdirSync(downloadsPath)
+            return fs.readdirSync(downloadsPath);
           } catch (err) {
-            console.error("Error reading downloads:", err)
+            console.error("Error reading downloads:", err);
             return null;
           }
         },
-      });
-
-      
-      // delete files in download 
-      on('task', {
-        clearDownloads() {
+        clearDownloads: () => {
           try {
             const downloadsFolder = path.join(config.projectRoot, 'cypress', 'downloads');
             fs.readdirSync(downloadsFolder).forEach((file) => {
               const filePath = path.join(downloadsFolder, file);
               fs.unlinkSync(filePath);
-            })
-          return null;
+            });
+            return null;
           } catch (err) {
             console.error("Error clearing downloads:", err);
             return null;
           }
         },
-      });
-      
-      // exec
-      on('task', {
         async execute(command) {
           return new Promise((resolve, reject) => {
-            try {
-              // Execute the command using exec with the shell option set to true
-              exec(command, { shell: 'bash' }, (error, stdout, stderr) => {
-                if (error) {
-                  // If there's an error, reject the promise with the error object
-                  reject(error);
-                } else {
-                  // If the command executed successfully, resolve the promise with the output
-                  resolve({ stdout, stderr });
-                }
-              });
-            } catch (e) {
-              // Catch any synchronous errors and reject the promise with the error
-              reject(e);
-            }
+            exec(command, { shell: 'bash' }, (error, stdout, stderr) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve({ stdout, stderr });
+              }
+            });
           });
         }
       });
-      
-      return config; // Return the updated configuration
 
-    },
-
-    chromeWebSecurity: false,
-    numTestsKeptInMemory: 0,
-    defaultCommandTimeout: 10000,
-    execTimeout: 60000,
-    pageLoadTimeout: 60000,
-    requestTimeout: 60000,
-    responseTimeout: 60000,
-    taskTimeout: 60000
-  },
+      return config;
+    }
+  }
 });
 
-// Database query function
 function queryTestDb(query, config) {
-  const connection = mysql.createConnection(config.env.db); // Ensure this matches your structure
-  
+  const connection = mysql.createConnection(config.env.db);
+
   return new Promise((resolve, reject) => {
     connection.connect();
 
     connection.query(query, (error, results) => {
       connection.end();
-      
+
       if (error) {
         reject(error);
       } else {
         resolve(results);
       }
-    })
-  })
+    });
+  });
 }
-
